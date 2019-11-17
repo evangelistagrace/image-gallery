e0730615d54d417d96e8053f819b88cabbde9605
Evangelista Grace <grace.evangrace@gmail.com>
2019-11-18 03:25:14 +0800

    initial commit

---
+++ src/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Image Gallery</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <div class="gallery">
+        <div class="lightbox"></div>
+        <div class="col-4">
+            <img src="https://picsum.photos/200?mountain" alt="">
+            <img src="https://picsum.photos/200?valley" alt="">
+            <img src="https://picsum.photos/200?dog" alt="">
+        </div>
+        <div class="col-4">
+            <img src="https://picsum.photos/200?sunset" alt="">
+            <img src="https://picsum.photos/200?beach" alt="">
+            <img src="https://picsum.photos/200?bear" alt="">
+        </div>
+        <div class="col-4">
+            <img src="https://picsum.photos/200?plant" alt="">
+            <img src="https://picsum.photos/200?cat" alt="">
+            <img src="https://picsum.photos/200?city" alt="">
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ src/my-favorites.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Favorites</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>hgvddd
+    <div class="gallery">
+        <div class="lightbox">
+            <div class="col-4">
+            <img src="https://picsum.photos/200?mountain" alt="">
+            <img src="https://picsum.photos/200?valley" alt="">
+            <img src="https://picsum.photos/200?dog" alt="">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
