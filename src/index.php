88152fdcd460db12ede9b58dbbfac35074714290
Evangelista Grace <grace.evangrace@gmail.com>
2019-11-23 23:35:00 +0800

    front end

--- src/index.php
+++ src/index.php
@@ -7,27 +7,95 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Image Gallery</title>
     <link rel="stylesheet" href="styles.css">
+    <!-- bootstrap -->
+    
 </head>
 
 <body>
+    <div class="navbar">
+        <div class="title">
+            Image Gallery
+        </div>
+        <ul class="links">
+            <li class="item"><a href="./my-favorites.php">My Favorites</a></li>
+            <li class="item"><a href="#">Log out</a></li>
+        </ul>
+    </div>
+    
     <div class="gallery">
         <div class="lightbox"></div>
         <div class="col-4">
-            <img src="https://picsum.photos/200?mountain" alt="">
-            <img src="https://picsum.photos/200?valley" alt="">
-            <img src="https://picsum.photos/200?dog" alt="">
+            <div class="img-container">
+                <img src="https://picsum.photos/200?mountain" alt="">
+                <div class="overlay">
+                    <a href="#"><i class="fas fa-heart"></i></a>
+                </div>
+            </div>
+
+            <div class="img-container">
+                <img src="https://picsum.photos/200?valley" alt="">
+                <div class="overlay">
+                    <a href="#"><i class="fas fa-heart"></i></a>
+                </div>
+            </div>
+
+            <div class="img-container">
+                <img src="https://picsum.photos/200?dog" alt="">
+                <div class="overlay">
+                    <a href="#"><i class="fas fa-heart"></i></a>
+                </div>
+            </div>
+
+
         </div>
         <div class="col-4">
-            <img src="https://picsum.photos/200?sunset" alt="">
-            <img src="https://picsum.photos/200?beach" alt="">
-            <img src="https://picsum.photos/200?bear" alt="">
+            <div class="img-container">
+                <img src="https://picsum.photos/200?sunset" alt="">
+                <div class="overlay">
+                    <a href="#"><i class="fas fa-heart"></i></a>
+                </div>
+            </div>
+
+            <div class="img-container">
+                <img src="https://picsum.photos/200?beach" alt="">
+                <div class="overlay">
+                    <a href="#"><i class="fas fa-heart"></i></a>
+                </div>
+            </div>
+
+            <div class="img-container">
+                <img src="https://picsum.photos/200?bear" alt="">
+                <div class="overlay">
+                    <a href="#"><i class="fas fa-heart"></i></a>
+                </div>
+            </div>
+
         </div>
         <div class="col-4">
-            <img src="https://picsum.photos/200?plant" alt="">
-            <img src="https://picsum.photos/200?cat" alt="">
-            <img src="https://picsum.photos/200?city" alt="">
+            <div class="img-container">
+                <img src="https://picsum.photos/200?plant" alt="">
+                <div class="overlay">
+                    <a href="#"><i class="fas fa-heart"></i></a>
+                </div>
+            </div>
+
+            <div class="img-container">
+                <img src="https://picsum.photos/200?cat" alt="">
+                <div class="overlay">
+                    <a href="#"><i class="fas fa-heart"></i></a>
+                </div>
+            </div>
+
+            <div class="img-container">
+                <img src="https://picsum.photos/200?city" alt="">
+                <div class="overlay">
+                    <a href="#"><i class="fas fa-heart"></i></a>
+                </div>
+            </div>
         </div>
     </div>
+
+    <script src="https://kit.fontawesome.com/a256fe27cf.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
